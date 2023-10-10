439111267b4a025c06d04bb9e019160f5bb3d1d5
Thacio Alves <thaccio.alves@gmail.com>
2023-10-10 18:55:25 -0300

    finish

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('extrato/', include('extrato.urls')),
     path('planejamento/', include('planejamento.urls')),
     path('contas/', include('contas.urls')),
+    path('', lambda request: redirect('/perfil/home/'))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
